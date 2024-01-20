8f924d9c781e8d4def24724dece0aa675da449b1
creativehimel <himel.phy@bsmrstu.edu.bd>
2024-01-21 00:56:33 +0600

    User Register functionality and error handeling complete

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -11,6 +11,10 @@ export const useAuthStore = defineStore('auth', () => {
   const isLoader = ref(false)
   const isAuthenticated = ref(false)
 
+  if (JSON.parse(localStorage.getItem('isAuthenticated'))) {
+    isAuthenticated.value = JSON.parse(localStorage.getItem('isAuthenticated'))
+  }
+
   // User Login Auth Store function
   async function userLogin(loginData) {
     isLoader.value = true
@@ -70,9 +74,60 @@ export const useAuthStore = defineStore('auth', () => {
     isLoader.value = false
   }
 
+  // User Registration Auth Store function
+  async function userRegistration(userData) {
+    isLoader.value = true
+    await axios
+      .post(`${baseURL}/register`, {
+        first_name: userData.firstName,
+        last_name: userData.lastName,
+        phone: userData.phone,
+        email: userData.email,
+        password: userData.password
+      })
+      .then((res) => {
+        toast(res.data.message, {
+          type: 'success',
+          transition: 'zoom',
+          dangerouslyHTMLString: true
+        })
+        setTimeout(() => {
+          router.push('/login')
+        }, 1000)
+      })
+      .catch((error) => {
+        if (error.response) {
+          if (error.response.status == 422) {
+            let errorList = error.response.data.message
+            for (let key in errorList) {
+              toast(errorList[key][0], {
+                type: 'error',
+                transition: 'zoom',
+                dangerouslyHTMLString: true
+              })
+            }
+          }
+        } else if (error.request) {
+          toast('No response received from the server.', {
+            type: 'error',
+            transition: 'zoom',
+            dangerouslyHTMLString: true
+          })
+        } else {
+          toast(error.message, {
+            type: 'error',
+            transition: 'zoom',
+            dangerouslyHTMLString: true
+          })
+        }
+      })
+    isLoader.value = false
+  }
+
   return {
     isLoader,
     isAuthenticated,
-    userLogin
+    userLogin,
+    userRegistration
   }
 })
--- src/views/Auth/Registration.vue
+++ src/views/Auth/Registration.vue
@@ -1,8 +1,123 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+const auth = useAuthStore()
+
+const userData = reactive({
+  firstName: '',
+  lastName: '',
+  phone: '',
+  email: '',
+  password: ''
+})
+</script>
 <template>
-  <div>
-    <h1>This is Registration page</h1>
-  </div>
+  <section class="bg-slate-100 px-4 py-16 md:py-28 flex items-center justify-center">
+    <div class="bg-white max-w-3xl w-full rounded p-5">
+      <from method="POST" action="#">
+        <h2 class="text-center text-2xl font-[700] pt-5">Create New Account</h2>
+        <h4 class="text-center text-sm font-[500] py-2">Get your free velzon account now</h4>
+        <div class="px-5 py-4">
+          <div class="flex flex-col md:flex-row md:gap-6 items-center">
+            <div class="py-2 w-full flex flex-col gap-y-2">
+              <label for="email">First Name</label>
+              <input
+                v-model="userData.firstName"
+                type="text"
+                class="border px-4 py-1.5 rounded-sm focus:none"
+                id="email"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div class="py-2 w-full flex flex-col gap-y-2">
+              <label for="email">Last Name</label>
+              <input
+                v-model="userData.lastName"
+                type="text"
+                class="border px-4 py-1.5 rounded-sm focus:none"
+                id="email"
+                placeholder="Enter your email"
+              />
+            </div>
+          </div>
+          <div class="flex flex-col md:flex-row md:gap-6 items-center">
+            <div class="py-2 w-full flex flex-col gap-y-2">
+              <label for="email">Phone Number</label>
+              <input
+                v-model="userData.phone"
+                type="text"
+                class="border px-4 py-1.5 rounded-sm focus:none"
+                id="email"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div class="py-2 w-full flex flex-col gap-y-2">
+              <label for="email">Email</label>
+              <input
+                v-model="userData.email"
+                type="text"
+                class="border px-4 py-1.5 rounded-sm focus:none"
+                id="email"
+                placeholder="Enter your email"
+              />
+            </div>
+          </div>
+          <div class="flex gap-6 items-center">
+            <div class="py-2 w-full flex flex-col gap-y-2">
+              <label for="password">Password</label>
+              <input
+                v-model="userData.password"
+                type="password"
+                class="border px-4 py-1.5 rounded-sm focus:none"
+                id="password"
+                placeholder="********"
+              />
+            </div>
+          </div>
+
+          <button
+            v-if="!auth.isLoader"
+            @click.prevent="auth.userRegistration(userData)"
+            class="bg-indigo-600 w-full mt-4 py-2 rounded text-white font-semibold hover:bg-indigo-700 duration-200 ring-1 ring-indigo-400"
+          >
+            Register
+          </button>
+          <button
+            v-else
+            type="button"
+            class="inline-flex w-full mt-4 justify-center items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150 cursor-not-allowed"
+            disabled=""
+          >
+            <svg
+              class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                class="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              ></circle>
+              <path
+                class="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Processing...
+          </button>
+        </div>
+      </from>
+      <p class="text-center py-1.5">
+        Don't have an account ?
+        <RouterLink to="/register" class="pl-2 text-red-600">Signup</RouterLink>
+      </p>
+    </div>
+  </section>
 </template>
 
 <style scoped></style>
