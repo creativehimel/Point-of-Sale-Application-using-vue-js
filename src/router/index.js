480c10719c5faca85591fe97f4d7b24c808407d7
creativehimel <himel.phy@bsmrstu.edu.bd>
2024-01-21 00:29:35 +0600

    All need pages and route added

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import NProgress from 'nprogress'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import Contact from '@/views/Contact.vue'
+import Blog from '@/views/Blog.vue'
+import FAQ from '@/views/FAQ.vue'
+import Login from '@/views/Auth/Login.vue'
+import Registration from '@/views/Auth/Registration.vue'
+import ForgotPassword from '@/views/Auth/ForgotPassword.vue'
+import VerifyOTP from '@/views/Auth/VerifyOTP.vue'
+import ResetPassword from '@/views/Auth/ResetPassword.vue'
+import Dashboard from '@/views/Admin/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,8 +19,71 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/about-us',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/contact-us',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/blog',
+      name: 'Blog',
+      component: Blog
+    },
+    {
+      path: '/faq',
+      name: 'FAQ',
+      component: FAQ
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Registration
+    },
+    {
+      path: '/forgot-password',
+      name: 'ForgotPassword',
+      component: ForgotPassword
+    },
+    {
+      path: '/verify-otp',
+      name: 'VeriftOTP',
+      component: VerifyOTP
+    },
+    {
+      path: '/reset-password',
+      name: 'ResetPassword',
+      component: ResetPassword
+    },
+    {
+      path: '/admin/dashboard',
+      name: 'Dashboard',
+      component: Dashboard
     }
   ]
+})
+
+router.beforeResolve((to, from, next) => {
+  // If this isn't an initial page load.
+  if (to.name) {
+    // Start the route progress bar.
+    NProgress.start()
+  }
+  next()
+})
+router.afterEach(() => {
+  // Complete the animation of the route progress bar.
+  NProgress.done()
 })
 
 export default router
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+<template>
+  <div>
+    <h1>This is about page</h1>
+  </div>
+</template>
+
+<style scoped></style>
